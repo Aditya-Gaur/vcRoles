500aca9cc43c428aa646f27e1f475fa6a8083e59
Aditya Gaur <75514601+Aditya-Gaur@users.noreply.github.com>
2023-01-04 13:32:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = "UR_bot_token"
+ROLE_NAME = "studying" # Role to add
+REMOVE_ROLE = "carbon-based life"   # Role to remove
+CATEGORY = "" # Category to monitor [leave empty if there are no specifications] 
---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+from discord.utils import get
+from discord.ext.commands import Bot
+from config import TOKEN, ROLE_NAME, REMOVE_ROLE, CATEGORY
+
+bot = Bot(command_prefix="*", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print('Now logged in as {0.user}'.format(bot))  
+   
+@bot.event
+async def on_voice_state_update(member, before, after):
+    guild = member.guild
+
+    if not get(guild.roles, name=ROLE_NAME): # Create role name if it doesnt exist
+        await guild.create_role(name=ROLE_NAME)
+
+    if CATEGORY == '':
+        if after.channel is None:
+            await member.remove_roles(discord.utils.get(member.guild.roles, name=ROLE_NAME))
+            await member.add_roles(discord.utils.get(member.guild.roles, name=REMOVE_ROLE))
+
+        elif not before.channel and after.channel:
+            await member.add_roles(discord.utils.get(member.guild.roles, name=ROLE_NAME))
+            await member.remove_roles(discord.utils.get(member.guild.roles, name=REMOVE_ROLE))
+
+    else:
+        if after.channel is None:
+            
+            category = discord.utils.get(guild.categories, id = int(CATEGORY))
+            if before.channel in category.channels:
+                await member.remove_roles(discord.utils.get(member.guild.roles, name=ROLE_NAME))
+                await member.add_roles(discord.utils.get(member.guild.roles, name=REMOVE_ROLE))
+
+
+        elif not before.channel and after.channel:
+
+            category = discord.utils.get(guild.categories, id = int(CATEGORY))
+            if after.channel in category.channels:
+                await member.add_roles(discord.utils.get(member.guild.roles, name=ROLE_NAME))
+                await member.remove_roles(discord.utils.get(member.guild.roles, name=REMOVE_ROLE))
+
+bot.run(TOKEN)
